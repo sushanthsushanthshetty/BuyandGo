14df23e7930134f98f17e5342bc2b7145f11c68a
Sushant <sushanthsushanthshetty5@gmail.com>
2025-04-27 16:44:39 +0530

    connected firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,22 @@
 import 'package:flutter/material.dart';
-import 'login.dart'; // Import the login screen
+import 'package:firebase_core/firebase_core.dart';
+import 'package:pppp/HomePage.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SignInPage(), // Set LoginScreen as the home screen
+      title: 'Firebase Auth Demo',
+      home: HomePage(),
     );
   }
 }
-
-
-
-// trial
